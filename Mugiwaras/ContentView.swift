5a154f42b451b2988dcb68d6b7d7a1fa10c6e0ef
Daniel Alcudia <angel_alcudia1@hotmail.com>
2020-11-19 00:04:38 -0600

    Initial Commit

---
+++ Mugiwaras/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mugiwaras
+//
+//  Created by Angel Daniel Alcudia on 19/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mugiwaras/MugiwarasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MugiwarasApp.swift
+//  Mugiwaras
+//
+//  Created by Angel Daniel Alcudia on 19/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MugiwarasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
